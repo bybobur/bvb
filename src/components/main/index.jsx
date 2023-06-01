e056e6a5d496ecf1b2028c158d9ab58bd21b23bb
Bobur Atakulov <boburatakulov@Boburs-MacBook-Pro.local>
2023-06-01 15:44:26 +0500

    main componentda ochildi va uni ichida index hamda style fayllari yaratildi

---
+++ src/components/main/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style";
+function Main() {
+  return (
+    <div>
+      <h1>salom</h1>
+      <h3>hello world</h3>
+      <button>enter</button>
+      <input type="text" name="" id="" />
+      <h6>helllooooo</h6>
+    </div>
+  );
+}
+
+export default Main;
